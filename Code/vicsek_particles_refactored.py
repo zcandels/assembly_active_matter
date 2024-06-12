f8d6333c60ca61fc1ea8cb0fac1f008ca2c9b756
Ariel Zachary Candelaria (PGR) <2941737c@student.gla.ac.uk>
2024-06-12 15:52:47 +0100

    Name changes (see description)
    In a previous commit I inadvertently removed the flat version of the program and renamed the modular version of the program to be 'vicsek_particles_flat.py'. I've since recovered the real flat version and renamed the modular version to 'vicsek_particles_refactored.py'

--- Code/vicsek_particles_refactored.py
+++ Code/vicsek_particles_refactored.py
@@ -45,14 +45,30 @@ class Particle():
     def set_topBottom_collision_flag(self, n):
         self.recent_top_bottom_collision = n
         return None
+    
+    def PBC(self, n, L_x, L_y):
+        p_x = self.position[n][0]
+        p_y = self.position[n][1]
         
-    def get_current_position(self, n):
+        if (p_x < -L_x * 0.5):
+            p_x = p_x + L_x
+        if (p_x >= L_x * 0.5):
+            p_x = p_x - L_x
+        if (p_y < - L_y * 0.5):
+            p_y = p_y + L_y
+        if (p_y >= L_y * 0.5):
+            p_y = p_y - L_y
+        
+        new_position = np.array([p_x, p_y])
+        self.position[n] = new_position
+        
+    def get_position(self, n):
         return self.position[n]
     
-    def get_current_velocity(self, n):
+    def get_velocity(self, n):
         return self.velocity[n]
     
-    def get_current_angle(self, n):
+    def get_angle(self, n):
         return self.angle[n]
     
     def get_num_steps(self):
@@ -60,19 +76,20 @@ class Particle():
         return len(position)
             
         
-def initialize_particles(v, Nparticles):
+def initialize_particles(v, L_x, Nparticles):
     Particles = {}
     for i in range(Nparticles):
-        r0 = np.random.uniform(-10, 10, 2)
+        r0 = np.random.uniform(-L_x, L_x, 2)
         angle0 = np.random.uniform(0, 2*np.pi)
         v0 = v*np.array([ np.cos(angle0), np.sin(angle0) ])
         Particles[i] = Particle(r0, v0, angle0)
     return Particles          
 
 
-def do_timestep(T, dt, v, Particles, interaction_radius, eta):
+def do_timestep(T, dt, L_x, L_y, v, Particles, interaction_radius, eta):
     # Time-stepping
     Nt = int(round(T/float(dt)))
+    Np = len(Particles)
     t = 0
 
     avg_normed_vel = 0
@@ -80,61 +97,71 @@ def do_timestep(T, dt, v, Particles, interaction_radius, eta):
     for n in range(Nt-1):
         t += dt
         
-        for i in Particles:
+        sum_vels = np.array([0., 0.])
+        for i in range(Np):
             p = Particles[i]
             
-            p_pos = p.get_current_position(n)
-            p_vel = p.get_current_velocity(n)
-            
-            d_pos = p_vel*dt
-            
-            p.set_position(p_pos + d_pos)
-            
-            v = np.linalg.norm(p_vel)
+            p_pos = p.get_position(n)
             
             avg_angle_p = 0
             ctr = 0
-            for j in Particles:
+            for j in range( Np ):
+                if i == j:
+                    continue
                 q = Particles[j]
-                q_pos = q.get_current_position(n)
+                q_pos = q.get_position(n)
                 
-                dist = np.linalg.norm( p_pos - q_pos )
-                if dist < interaction_radius:
+                pq_dist = p_pos - q_pos
+                [x_dist, y_dist] = pq_dist
+
+                dx = x_dist - L_x * round(x_dist/L_x)
+                dy = y_dist - L_y * round(y_dist/L_y)
+
+                r_pq2 = dx*dx + dy*dy
+                if r_pq2 < interaction_radius*interaction_radius:
+                    #print("interaction ", ctr)
                     ctr += 1
-                    avg_angle_p += q.get_current_angle(n)
+                    avg_angle_p += q.get_angle(n)
             
+            avg_angle_p += p.get_angle(n)
             avg_angle_p /= ctr+1
             d_angle = np.random.uniform(-eta/2, eta/2)
             angle_np1 = avg_angle_p + d_angle
             p.set_angle(angle_np1)
             
             vel_np1 = v*np.array( [np.cos(angle_np1), np.sin(angle_np1)] )
+            #print(np.linalg.norm(vel_np1))
             p.set_velocity(vel_np1)
-            avg_normed_vel += vel_np1
-        avg_normed_vel = np.linalg.norm(avg_normed_vel)/len(Particles)
-        
-        
-            
+            sum_vels += vel_np1
 
-def output_results(Particles, domLen):
-        [fig,ax] = plt.subplots()
+            p_vel = p.get_velocity(n)
+            d_pos = p_vel*dt
+            p.set_position(p_pos + d_pos)
+            p.PBC(n, L_x, L_y)
+            
+        avg_normed_vel = np.linalg.norm(sum_vels)/(Np*v)
+        return avg_normed_vel
         
     
-def main():
-    domLen = 1
-    rho = 50
-    N_particles = int(rho*domLen**2)
+def run_simulator():
+    L_x = 10
+    L_y = 10
+    N_particles = 400
+    rho = N_particles/(L_x*L_y)
     dt = 1
-    T = 100
+    T = 10000
     v = 0.03
     interaction_radius= 1
-    eta = 0.1
+    eta = 5
 
-    Particles = initialize_particles(v, N_particles)
+    Particles = initialize_particles(v, L_x, N_particles)
     
-    do_timestep(T, dt, domLen, Particles, interaction_radius, eta)
-    
-    output_results(Particles, domLen)
+    v_a = do_timestep(T, dt, L_x, L_y, v,
+                      Particles, interaction_radius, eta)
+    print(v_a)
+
+def main():
+    run_simulator()
     
 if __name__ == "__main__":
     main()
